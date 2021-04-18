e1c8ec14db7e23459ba962830d29697dc75d9580
Wiktor Obrębski <psychowico@gmail.com>
2021-04-18 17:35:51 +0200

    Add JsDoc to the Result/Ok/Err interfaces

--- src/result-helpers.ts
+++ src/result-helpers.ts
@@ -22,6 +22,12 @@ type Ok<TValue> = Subresult & {
 
   __brand: 'ok';
 
+  /**
+   * Return fulfilled promise of current Ok Result value. To use the `promise()`
+   * function you need first handle all known Err result values.
+   * It is possible that it return rejected promise for unknown exceptions.
+   * @returns promise of current Result value - fulfilled if the value is Ok, rejected if the was an exception throw in the mapping chain
+   */
   promise<U extends Result<unknown, unknown>>(
     this: U
   ): Promise<InferOk<U> | never>;
@@ -34,22 +40,48 @@ type Err<TError> = Subresult & {
 };
 
 interface Subresult {
+  /**
+   * Map current Result if it's Ok. It's left Err Result unchanged.
+   * You can use async function for mapping.
+   * @param mapper the function used to map the current Ok result, can be async
+   * @returns new Result, mapped if current Result is Ok, left unchanged otherwise
+   */
   map<U extends Result<unknown, unknown>, R>(
     this: U,
     mapper: OkMapper<U, R>
   ): SpreadErrors<MapOkResult<SpreadErrors<U>, R>>;
 
+  /**
+   * Map current Result if it's a Err of a specific class.
+   * It's left Ok Result and Err of different class unchanged.
+   * You can use async function for mapping.
+   * @param ErrorClass the class of specific error object that should be mapped
+   * @param mapper the function used to map the current Err result, can be async
+   * @returns new Result, mapped if current Result is Err of specific class, left unchanged otherwise
+   */
   mapErr<U extends Result<unknown, unknown>, R, E extends InferErr<U>>(
     this: U,
     ErrorClass: AClass<E>,
     mapper: (err: E) => R
   ): SpreadErrors<MapErrResult<SpreadErrors<U>, R, E>>;
 
+  /**
+   * Map current Result if it's Err. It's left Ok Result unchanged.
+   * You can use async function for mapping.
+   * @param mapper the function used to map the current Err result, can be async
+   * @returns new Result, mapped if current Result is Err, left unchanged otherwise
+   */
   mapAnyErr<U extends Result<unknown, unknown>, R>(
     this: U,
     mapper: ErrMapper<U, R>
   ): SpreadErrors<MapAnyErrResult<SpreadErrors<U>, R>>;
 
+  /**
+   * WARNING: it's not recommended to use this function. consider using `promise()` instead.
+   * Return promise of current Result value - fulfilled if the value is Ok,
+   * rejected if the value is Err. The Err type will be lost in the process.
+   * @returns promise of current Result value - fulfilled if the value is Ok, rejected if the value is Err
+   */
   unsafePromise<U extends Result<unknown, unknown>>(
     this: U
   ): Promise<InferOk<U> | never>;
--- src/result.ts
+++ src/result.ts
@@ -1,16 +1,25 @@
 import { CommonResult } from './common-result';
 import { Result as ResultType, Ok, Err, SpreadErrors } from './result-helpers';
 
-export { Ok, Err, Result, CombineResult };
+export { Ok, OkNamespace, Err, ErrNamespace, Result, ResultNamespace };
 
-type CombineResult<
-  T extends readonly ResultType<unknown, unknown>[]
-> = SpreadErrors<ResultType<ExtractOkTypes<T>, ExtractErrTypes<T>[number]>>;
-
-const Result = {
+interface ResultNamespace {
+  /**
+   * Combine provided Results list into single Result. If all provided Results
+   * are Ok Results, returned Result will be Result Ok of array of provided Results values:
+   * [Ok<A>, Ok<B>] -> Ok<[A, B]>
+   * If provided Results list have at least one Err Result, returned Result will be
+   * Result Err of first Err value found in the array.
+   * @param results list of Ok and/or Err Results
+   * @returns Result Ok of all input Ok values, or Err Result of one of provided Err values.
+   */
   combine<T extends readonly ResultType<unknown, unknown>[]>(
     results: [...T]
-  ): CombineResult<T> {
+  ): SpreadErrors<ResultType<ExtractOkTypes<T>, ExtractErrTypes<T>[number]>>;
+}
+
+const Result: ResultNamespace = {
+  combine(results) {
     const wrappersPromise = Promise.all(results.map((res) => res.__value));
     const resultPromise = wrappersPromise.then((wrappers) => {
       const values = [];
@@ -28,15 +37,33 @@ const Result = {
   },
 };
 
-const Ok = {
-  of<TValue>(value: TValue) {
+interface OkNamespace {
+  /**
+   * Return Ok Result of the provided value
+   * @param value the value for which Ok Result should be returned
+   * @returns Ok Result of provided value
+   */
+  of<TValue>(value: TValue): Ok<TValue>;
+}
+
+interface ErrNamespace {
+  /**
+   * Return Err Result of the provided value
+   * @param error the error for which Err Result should be returned
+   * @returns Err Result of provided error instance
+   */
+  of<TError>(error: TError): TError extends unknown ? Err<TError> : never;
+}
+
+const Ok: OkNamespace = {
+  of(value) {
     return CommonResult.ok(value);
   },
 };
 
-const Err = {
-  of<TError>(error: TError) {
-    return CommonResult.err(error);
+const Err: ErrNamespace = {
+  of(error) {
+    return CommonResult.err(error) as any;
   },
 };
 
