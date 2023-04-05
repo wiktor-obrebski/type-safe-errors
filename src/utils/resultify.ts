3dcb6c90bc593c4e67c19e81672f07f0280fa899
Marhw <marhw89@gmail.com>
2023-04-05 18:13:20 +0200

    feature/utils-resultify

---
+++ src/utils/resultify.ts
@@ -0,0 +1,10 @@
+import { Result } from '../result';
+
+export { resultify };
+
+type Callback<TArgs extends unknown[], TResult> = (...args: TArgs) => TResult;
+
+const resultify =
+  <TArgs extends unknown[], TResult>(fn: Callback<TArgs, TResult>) =>
+  (...args: TArgs) =>
+    Result.from(() => fn(...args));
